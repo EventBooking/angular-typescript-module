1e176034996a7e266afff81a9aef1e95084730b3
Mike Walters <mike@eventbooking.com>
2016-02-22 11:44:45 -0500

    refactor

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,27 @@
 /* global require, __dirname */
 var gulp = require('gulp'),
     project = require('./package.json'),
+    concat = require('gulp-concat'),
     dest = 'dist';
 
-gulp.task('stripRefs', stripRefs);
+gulp.task('postTsc', postTsc);
+gulp.task('clean', clean);
 
-function stripRefs() {
+function clean() {
+    var del = require('del');
+    return del(dest);
+}
+
+function stripRefs(src, dest, name) {
     var strip = require("gulp-strip-comments");
-    
-    return gulp.src(dest + '/angular-typescript-module.d.ts')
+
+    return gulp.src(src)
         .pipe(strip())
+        .pipe(concat(name))
         .pipe(gulp.dest(dest));
+}
+
+function postTsc() {
+    stripRefs(dest + '/angular-typescript-module.debug.d.ts', dest, '/angular-typescript-module.d.ts');
+    stripRefs(dest + '/angular-typescript-module.debug.js', dest, '/angular-typescript-module.js');
 }
