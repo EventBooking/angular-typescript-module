0290723ae7f6bf8c7075c758d6faf2c11da6cce8
Mike Walters <mike@eventbooking.com>
2017-02-16 16:53:59 -0500

    commonjs2

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, "dist"),
     filename: '[name].js',
-    library: "Angular"
+    libraryTarget: "commonjs2"
   },
   resolve: {
     extensions: ['.ts', '.js']
