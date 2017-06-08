0434cc239839e9cbf64bd31dd030a0de26dede2f
Mike Walters <mike@eventbooking.com>
2017-06-08 11:04:55 -0400

    closes #1

--- karma.conf.js
+++ karma.conf.js
@@ -24,8 +24,8 @@ module.exports = function(config) {
             'dist/**/*.css',
             'demo/**/*.css',
             
-            'bower_components/jquery/dist/jquery.js',
-            'bower_components/angular/angular.js',
+            'node_modules/jquery/dist/jquery.js',
+            'node_modules/angular/angular.js',
             'node_modules/angular-mocks/angular-mocks.js',
             
             'dist/**/*.js',
